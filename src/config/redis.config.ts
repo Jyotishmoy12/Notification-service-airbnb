bcbf0788295a03f48d00e2effe9632dbc46d180a
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-08-28 11:44:28 +0530

    added redis setup and sets up basic queues using bullmq and redis

---
+++ src/config/redis.config.ts
@@ -0,0 +1,32 @@
+import Redis from "ioredis";
+import { serverConfig } from ".";
+
+function connectToRedis() {
+  try {
+    let connection:Redis;
+    const redisConfig = {
+      port: serverConfig.REDIS_PORT,
+      host: serverConfig.REDIS_HOST,
+    }; 
+
+    return () =>{
+        if(!connection){
+            connection = new Redis(redisConfig);
+            return connection;
+        }
+        return connection;
+    }
+
+    
+  } catch (error) {
+    console.error("Error connecting to Redis:", error);
+    throw error;
+  }
+}
+// Singleton pattern to ensure a single Redis connection instance
+// throughout the application lifecycle
+// when we call getRedisConnectionObject it will return the connectToRedis function
+// then we call that function it will return the Redis connection instance
+// if the connection instance is not created it will create one and return it
+// if the connection instance is already created it will return the existing one
+export const getRedisConnectionObject = connectToRedis();
